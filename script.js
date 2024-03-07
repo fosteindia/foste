093a0f5fc38d1cf31a26438d2fd2231d402c4666
fosteindia <162029911+fosteindia@users.noreply.github.com>
2024-03-07 14:48:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function(){//window.addEventListener('load',function(){...}); (for Netscape) and window.attachEvent('onload',function(){...}); (for IE and Opera) also work alert('<%: TempData["Resultat"]%>'); }
+
 
 
 $(document).ready(function(){
