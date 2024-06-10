bc7c82879f0470b6a2ddb83d1ba41caff5143d05
fosteindia <162029911+fosteindia@users.noreply.github.com>
2024-06-10 21:27:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -99,14 +99,3 @@ $(".carousel").owlCarousel({
     });
 
 
-window.addEventListener("load", function(){
-    setTimeout(
-        function open(event){
-            document.querySelector(".popup").style.display = "block";
-        },
-        1000
-    )
-});
-document.querySelector("#close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
-});
