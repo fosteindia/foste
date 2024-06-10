944c37476b0ec24adb83176cdb18b4e64b57d9f4
fosteindia <162029911+fosteindia@users.noreply.github.com>
2024-06-10 21:25:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -97,3 +97,16 @@ $(".carousel").owlCarousel({
         }
       }
     });
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
